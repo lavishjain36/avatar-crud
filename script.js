18179abf6fd5d5b205064d6e6dc28b479266cdaf
Lavish Jain <lavishjain36@gmail.com>
2022-01-02 12:22:01 +0530

    Crud avatar

--- script.js
+++ script.js
@@ -1,76 +1,97 @@
-var url = "https://61d12cbecd2ee50017cc99a4.mockapi.io/users";
-
-//HTTP METHOD=GET
-//Read the data
-function getData() {
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+//Write a logic to get the data
+async function getUsers() {
+  let users;
+  try {
+    const data = await fetch(
+      "https://61d12cbecd2ee50017cc99a4.mockapi.io/users",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    users = await data.json();
+    // console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+  return users;
 }
-// getData();
+// getUsers();
 
-//HTTP METHOD=POST
-//Create the data
-function createData() {
-  let data = {
-    name: "Eeshani",
-    email: "raja@gmail.in",
-  };
+//write a functionality to display the data of users
+async function displayUsers() {
+  let users = await getUsers();
+  // console.log(users);
+  const userList = document.querySelector(".user-list");
+  userList.innerHTML = "";
 
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-}
-// createData();
-// getData();
-
-//HTTP METHOD=PUT
-//Update the data
-function updateData() {
-  let data = {
-    name: "Lavish Kumar",
-    email: "lavish@gmail.com",
-  };
-
-  fetch(url + "/11", {
-    method: "PUT",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-}
-// updateData();
-// getData();
-
-//HTTP METHOD=DELETE
-//Delete the data
-function deleteData() {
-  fetch(url + "/15", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+  users.forEach((user) => {
+    // console.log(user.name);
+    //Load the data from an api using template literal
+    userList.innerHTML += `
+    <div class="user-container">
+    <img class="user-avatar" src="${user.avatar}" alt="${user.name}">
+    <div>
+    <h3>${user.name}</h3>
+    <p>${user.createdAt}</p>
+    <button onClick="deleteUser(${user.id})">Delete</button>
+    <button onClick="editUser(${user.id})">Edit</button>
+    </div>
+     
+  
+  </div>
+  
+  `;
+  });
 }
 
-deleteData();
+displayUsers();
+
+//Write a functionality to delete the user
+async function deleteUser(id) {
+  try {
+    const data = await fetch(
+      `https://61d12cbecd2ee50017cc99a4.mockapi.io/users/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const users = await data.json();
+    console.log(users);
+    displayUsers();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//use POST
+async function addUser() {
+  const userName = document.querySelector(".add-user-name").value;
+  const userAvatar = document.querySelector(".add-user-avatar").value;
+  // console.log(userName, userAvatar);
+  // 1.method=>POST
+  // 2.Stringify the data
+
+  const data = await fetch(
+    "https://61d12cbecd2ee50017cc99a4.mockapi.io/users",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: userName,
+        avatar: userAvatar,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  displayUsers();
+}
+addUser();
+
+// EDIT,PUT
