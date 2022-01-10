ebb1fc01e4680cf4b4c681a1d85bf87fbb92b1b6
Lavish Jain <lavishjain36@gmail.com>
2022-01-10 11:03:44 +0530

    Calc

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-let array = ["Lavish", "Mohammad", "Kushal", "Guvi"];
-console.log(array);
+function add() {
+  var num1 = document.getElementById("first").value;
+  var num2 = document.getElementById("second").value;
+  var result = parseInt(num1) + parseInt(num2);
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerHTML =
+    "Addition is " + result;
+}
+
+function sub() {
+  var num1 = document.getElementById("first").value;
+  var num2 = document.getElementById("second").value;
+  var result = parseInt(num1) - parseInt(num2);
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerHTML =
+    "Subtraction is " + result;
+}
+
+function mul() {
+  var num1 = document.getElementById("first").value;
+  var num2 = document.getElementById("second").value;
+  var result = parseInt(num1) * parseInt(num2);
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerText =
+    "Multiplication is " + result;
+}
+
+function div() {
+  var num1 = document.getElementById("first").value;
+  var num2 = document.getElementById("second").value;
+  var result = parseInt(num1) / parseInt(num2);
+  console.log(result);
+
+  document.getElementsByClassName("output")[0].innerText =
+    "Division is " + result;
+}
