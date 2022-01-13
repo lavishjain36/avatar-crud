7d9487b57c0ac82b6fbb7f220f11138f3af553cb
Lavish Jain <lavishjain36@gmail.com>
2022-01-13 11:30:25 +0530

    Dom-prac

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function myfun() {
-  let array = document.getElementsByTagName("p");
-  //   console.log(array);
-  for (let i = 0; i < array.length; i++) {
-    array[i].style.color = "red";
-    array[i].style.fontSize = "20px";
-  }
+//change the html using id selector
+function changeContent() {
+  //select the target
+  document.getElementById("heading").innerHTML = "Hello.Content is changed";
+}
+
+//changing font
+function getfont() {
+  document.getElementById("test").style.fontSize = "30px";
+}
+
+function toggleoff() {
+  document.getElementById("toggle").style.display = "none";
+}
+
+function toggleon() {
+  document.getElementById("toggle").style.display = "block";
+}
+
+function insert_row() {
+  let x = document.getElementById("sampletable").insertRow();
+  let y = x.insertCell(0);
+  let z = x.insertCell(1);
+
+  y.innerHTML = "New Row-1st cell";
+  z.innerHTML = "New Row-2nd cell";
 }
