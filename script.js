17e6fd7b3d6adc73160f81731b0574d0905e7a39
Lavish Jain <lavishjain36@gmail.com>
2022-01-13 12:05:10 +0530

    todo app using dom

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-//change the html using id selector
-function changeContent() {
-  //select the target
-  document.getElementById("heading").innerHTML = "Hello.Content is changed";
-}
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  //take the input value via input form
+  const text = document.querySelector(".text");
+  //   console.log(text.value);
 
-//changing font
-function getfont() {
-  document.getElementById("test").style.fontSize = "30px";
-}
-
-function toggleoff() {
-  document.getElementById("toggle").style.display = "none";
-}
-
-function toggleon() {
-  document.getElementById("toggle").style.display = "block";
-}
-
-function insert_row() {
-  let x = document.getElementById("sampletable").insertRow();
-  let y = x.insertCell(0);
-  let z = x.insertCell(1);
-
-  y.innerHTML = "New Row-1st cell";
-  z.innerHTML = "New Row-2nd cell";
-}
+  let newList = document.createElement("li");
+  newList.innerHTML = text.value;
+  let res = document.querySelector(".list");
+  console.log(res.appendChild(newList));
+});
