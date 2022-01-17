0860666e1e5af8431364011c975fe2b3c13d8998
Lavish Jain <lavishjain36@gmail.com>
2022-01-17 11:43:54 +0530

    Callback hell

--- script.js
+++ script.js
@@ -1,17 +1,71 @@
-const button = document.querySelector(".button");
+const time = 1000;
 
-button.addEventListener("click", function () {
-  //take the input the value
-  const text = document.getElementsByClassName("text")[0].value;
-  console.log(text);
-  //create a new li
-  const li = document.createElement("li");
-  //add the text to the li
-  // console.log(li);
-  li.innerText = text;
-  document.querySelector(".list").appendChild(li);
-  //clear the input
-  document.getElementsByClassName("text")[0].value = "";
+//Raising a dough
+function raiseDough(callback) {
+  console.log("Waiting for dough...");
+  setTimeout(() => {
+    console.log("Dough is ready!");
+    callback();
+  }, 3 * time);
+}
+
+//Rolling a dough
+function rollDough(callback) {
+  console.log("Rolling dough...");
+  setTimeout(() => {
+    console.log("Dough is rolled!");
+    callback();
+  }, 2 * time);
+}
+
+//pouring the sauce
+function pourSauce(callback) {
+  console.log("Pouring sauce...");
+  setTimeout(() => {
+    console.log("Sauce is poured!");
+    callback();
+  }, 1 * time);
+}
+
+//Add the topping
+function addTopping(callback) {
+  console.log("Adding topping...");
+  setTimeout(() => {
+    console.log("Topping is added!");
+    callback();
+  }, 1 * time);
+}
+
+function bakePizza(callback) {
+  console.log("Baking pizza...");
+  setTimeout(() => {
+    console.log("Pizza is baked!");
+    callback();
+  }, 8 * time);
+}
+
+//Deleivering the pizza
+function deliverPizza(callback) {
+  console.log("Delivering pizza...");
+  setTimeout(() => {
+    console.log("Pizza is delivered!");
+    callback();
+  }, 5 * time);
+}
+
+// We will Nested One inside another
+raiseDough(() => {
+  rollDough(() => {
+    pourSauce(() => {
+      addTopping(() => {
+        bakePizza(() => {
+          deliverPizza(() => {
+            console.log("Your pizzza id Deleivered Successfully");
+          });
+        });
+      });
+    });
+  });
 });
 
-// append=?string like input
+// Callback Hell
