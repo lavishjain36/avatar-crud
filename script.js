3b9471fc1db5a0e882c1c2f6a8f1a871c805a39c
Lavish Jain <lavishjain36@gmail.com>
2022-01-19 15:25:15 +0530

    callback ex

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
-//select the cat element
-const cat_btn = document.getElementById("cat_btn");
-const cat_result = document.getElementById("cat_result");
+let a, b, c, d;
 
-//select the dog element
-const dog_btn = document.getElementById("dog_btn");
-const dog_result = document.getElementById("dog_result");
-
-cat_btn.addEventListener("click", function () {
-  fetch("https://aws.random.cat/meow")
-    .then((response) => response.json())
-    .then((data) => {
-      cat_result.innerHTML = `<img src="${data.file}" alt="cat" width="300" height="250">`;
+PromptUser("Input First value", (err, value1) => {
+  if (err) {
+    return handleError(err);
+  }
+  a = Number(value1);
+  PromptUser("Input Second value", (err, value2) => {
+    if (err) {
+      return handleError(err);
+    }
+    b = Number(value2);
+    PromptUser("Input Third value", (err, value3) => {
+      if (err) {
+        return handleError(err);
+      }
+      c = Number(value3);
+      PromptUser("Input Fourth value", (err, value4) => {
+        if (err) {
+          return handleError(err);
+        }
+        d = Number(value4);
+        document.body.append(`The sum is ${a + b + c + d}`);
+      });
     });
+  });
 });
 
-dog_btn.addEventListener("click", function () {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      dog_result.innerHTML = `<img src="${data.message}" alt="dog" width="300" height="250">`;
-    });
-});
+function handleError(err) {
+  document.body.append(`Error ${err}`);
+}
+
+function PromptUser(question, callback) {
+  const wrapper = document.createElement("div");
+  const text = document.createElement("p");
+  const input = document.createElement("input");
+  const cancel = document.createElement("button");
+  const confirm = document.createElement("button");
+
+  confirm.innerHTML = "Ok";
+  cancel.innerHTML = "Cancel";
+  text.innerHTML = question;
+
+  wrapper.appendChild(text);
+  wrapper.appendChild(input);
+  wrapper.appendChild(cancel);
+  wrapper.appendChild(confirm);
+  document.body.appendChild(wrapper);
+
+  confirm.addEventListener("click", () => {
+    callback(undefined, input.value);
+    document.body.removeChild(wrapper);
+  });
+
+  cancel.addEventListener("click", () => {
+    callback(new Error("User canceled"), undefined);
+    document.body.removeChild(wrapper);
+  });
+}
