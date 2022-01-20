3785cdd8d7e12029c285150bff0c84c484847843
Lavish Jain <lavishjain36@gmail.com>
2022-01-20 10:54:49 +0530

    Quotes

--- script.js
+++ script.js
@@ -1,59 +1,23 @@
-let a, b, c, d;
+//select the target element
+let btn = document.getElementById("btn");
+let data = document.getElementById("data");
 
-PromptUser("Input First value", (err, value1) => {
-  if (err) {
-    return handleError(err);
+const url = "https://www.breakingbadapi.com/api/quote/random";
+
+//add an event listener to the target element
+btn.addEventListener("click", async function () {
+  data.innerHTML = "Loading....";
+  //write your logic here
+  try {
+    //fetch the data
+    let res = await fetch(url);
+    let object = await res.json();
+    // console.log(object[0].author);
+    data.innerHTML = `
+    <h1>${object[0].author}</h1>
+    <p>${object[0].quote}</p>
+    <p>${object[0].series}</p>`;
+  } catch (error) {
+    console.log(error);
   }
-  a = Number(value1);
-  PromptUser("Input Second value", (err, value2) => {
-    if (err) {
-      return handleError(err);
-    }
-    b = Number(value2);
-    PromptUser("Input Third value", (err, value3) => {
-      if (err) {
-        return handleError(err);
-      }
-      c = Number(value3);
-      PromptUser("Input Fourth value", (err, value4) => {
-        if (err) {
-          return handleError(err);
-        }
-        d = Number(value4);
-        document.body.append(`The sum is ${a + b + c + d}`);
-      });
-    });
-  });
 });
-
-function handleError(err) {
-  document.body.append(`Error ${err}`);
-}
-
-function PromptUser(question, callback) {
-  const wrapper = document.createElement("div");
-  const text = document.createElement("p");
-  const input = document.createElement("input");
-  const cancel = document.createElement("button");
-  const confirm = document.createElement("button");
-
-  confirm.innerHTML = "Ok";
-  cancel.innerHTML = "Cancel";
-  text.innerHTML = question;
-
-  wrapper.appendChild(text);
-  wrapper.appendChild(input);
-  wrapper.appendChild(cancel);
-  wrapper.appendChild(confirm);
-  document.body.appendChild(wrapper);
-
-  confirm.addEventListener("click", () => {
-    callback(undefined, input.value);
-    document.body.removeChild(wrapper);
-  });
-
-  cancel.addEventListener("click", () => {
-    callback(new Error("User canceled"), undefined);
-    document.body.removeChild(wrapper);
-  });
-}
