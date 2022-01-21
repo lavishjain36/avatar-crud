a54f44f1fbf88f85a5a924307aadbfeea9ed8319
Lavish Jain <lavishjain36@gmail.com>
2022-01-21 16:27:13 +0530

    Fetch-api random

--- script.js
+++ script.js
@@ -1,89 +1,33 @@
-var url = "https://61ea3a267bc0550017bc65e7.mockapi.io/users";
-
-// HTTP Method=>GET (READ THE DATA)
-function getData() {
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+//function to create an Html element
+function createNode(element) {
+  return document.createElement(element);
 }
 
-// getData();
-
-//HTTP Method=>Send the data (CREATE)
-// POST=>
-function createData() {
-  let data = {
-    name: "kohn",
-    email: "kohn@gmail.com",
-  };
-
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+//Function to append the element
+function append(parent, el) {
+  return parent.appendChild(el);
 }
 
-// createData();
-getData();
+//select the element
+const ul = document.getElementById("imglst");
+const url = "https://randomuser.me/api/?gender=male&results=100";
 
-// 3.Http Method:PUT
-// To update the data
-function updateData() {
-  let data = {
-    name: "Sindhu",
-    email: "sindhunana@gmail.com",
-  };
-  fetch(url + "/1", {
-    method: "PUT",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => {
+    let authors = data.results;
+    console.log(authors);
+    return authors.map((author) => {
+      let li = createNode("li");
+      let img = createNode("img");
+      let span = createNode("span");
+      img.src = author.picture.large;
+      // span.innerHTML = `${author.name.first} ${author.name.last}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
     });
-}
-
-updateData();
-
-// 4.Http Method:DELETE
-// To delete the data
-function deleteData() {
-  fetch(url + "/17", {
-    method: "DELETE",
   })
-    .then((result) => result.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-deleteData();
+  .catch((err) => {
+    console.log(err);
+  });
