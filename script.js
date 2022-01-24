48dddb48fcdb6a8fb2257197a70c3ffb23b1f692
Lavish Jain <lavishjain36@gmail.com>
2022-01-24 15:10:21 +0530

    crud

--- script.js
+++ script.js
@@ -1,33 +1,90 @@
-//function to create an Html element
-function createNode(element) {
-  return document.createElement(element);
-}
+var url = "https://61ee6cd4d593d20017dbae04.mockapi.io/users";
 
-//Function to append the element
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
-//select the element
-const ul = document.getElementById("imglst");
-const url = "https://randomuser.me/api/?gender=male&results=100";
-
-fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => {
-    let authors = data.results;
-    console.log(authors);
-    return authors.map((author) => {
-      let li = createNode("li");
-      let img = createNode("img");
-      let span = createNode("span");
-      img.src = author.picture.large;
-      // span.innerHTML = `${author.name.first} ${author.name.last}`;
-      append(li, img);
-      append(li, span);
-      append(ul, li);
-    });
+//Http method to read the data
+function getData() {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
   })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// getData();
+
+//POST method
+// send the data
+function createData() {
+  let data = {
+    name: "Saran",
+    email: "saran@gmail.in",
+  };
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// createData();
+
+// Update data=>PUT
+function updateData() {
+  let data = {
+    name: "Nijesh",
+    email: "nijesh@gmail.com",
+  };
+
+  fetch(url + "/1", {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// updateData();
+
+// Delete
+function deleteData() {
+  fetch(url + "/11", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+deleteData();
